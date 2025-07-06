cfa8d6d9b9673b9d23976838c9e698f576cd525c
Aryachiko <arya08092023@gmail.com>
2025-07-06 19:11:09 +0700

    carieditpengunjung.php

--- pengunjung/carieditpengunjung.php
+++ pengunjung/carieditpengunjung.php
@@ -7,17 +7,24 @@
 <center>
 <h1>MENAMPILKAN DATA PENGUNJUNG</h1>
 <hr>
-<form action="saveeditpengunjung.php" method="post"> <table border="0"> <?php
-require ("Koneksi.php");
+<form action="saveeditpengunjung.php" method="post"> <table> <?php
+require ("Koneksi.php"); // Memanggil Koneksi.php
 
 $nama_pengunjung_cari = $_POST['nama_pengunjung'];
+$foundVisitor = null;
 
-$sql = "SELECT id, nama_pengunjung, jenis_kelamin, tanggal_berkunjung, hobi, disukai FROM pengunjung WHERE nama_pengunjung = '$nama_pengunjung_cari'";
-$hasil = mysqli_query($conn, $sql);
-$row = mysqli_fetch_row($hasil);
+$visitors = readVisitors(); // Baca semua pengunjung
 
-if ($row) {
-    list ($id_db, $nama_pengunjung_db, $jenis_kelamin_db, $tanggal_berkunjung_db, $hobi_db, $disukai_db) = $row;
+foreach ($visitors as $visitor) {
+    // Cari berdasarkan nama pengunjung
+    if ($visitor['nama_pengunjung'] === $nama_pengunjung_cari) {
+        $foundVisitor = $visitor;
+        break;
+    }
+}
+
+if ($foundVisitor) {
+    list ($id_db, $nama_pengunjung_db, $jenis_kelamin_db, $tanggal_berkunjung_db, $hobi_db, $disukai_db) = array_values($foundVisitor);
     echo "<tr><td>ID Pengunjung</td><td><input type='text' name='id' value='" . htmlspecialchars($id_db) . "' readonly></td></tr>";
     echo "<tr><td>Nama Pengunjung</td><td><input type='text' name='nama_pengunjung' value='" . htmlspecialchars($nama_pengunjung_db) . "'></td></tr>";
     echo "<tr><td>Jenis Kelamin</td><td>";
@@ -27,15 +34,14 @@ if ($row) {
     echo "<tr><td>Tanggal Berkunjung</td><td><input type='date' name='tanggal_berkunjung' value='" . htmlspecialchars($tanggal_berkunjung_db) . "'></td></tr>";
     echo "<tr><td>Hobi</td><td><input type='text' name='hobi' value='" . htmlspecialchars($hobi_db) . "'></td></tr>";
     echo "<tr><td>Disukai</td><td><input type='text' name='disukai' value='" . htmlspecialchars($disukai_db) . "'></td></tr>";
+    echo "<tr><td></td><td><input type='submit' value='Save Edit'> <input type='reset' value='Reset'></td></tr>";
 } else {
-    echo "<tr><td colspan='2'>Data pengunjung dengan nama " . htmlspecialchars($nama_pengunjung_cari) . " tidak ditemukan.</td></tr>";
+    echo "<tr><td colspan='2'>Data pengunjung dengan nama <b>" . htmlspecialchars($nama_pengunjung_cari) . "</b> tidak ditemukan.</td></tr>";
 }
 
-mysqli_close($conn);
 ?>
 </table>
-<hr>
-<input type="submit" value="Save"> <input type="reset" value="Reset"> </form>
+</form>
 </center>
 </body>
 </html>
