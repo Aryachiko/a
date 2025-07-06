669ef4754b5a69c1ca75d7dcc659bd69520822f0
Aryachiko <arya08092023@gmail.com>
2025-07-06 19:11:43 +0700

    saveeditpengunjung.php

--- pengunjung/saveeditpengunjung.php
+++ pengunjung/saveeditpengunjung.php
@@ -9,8 +9,7 @@
 <hr width="300">
 <table>
 <?php
-require ("koneksi.php");
-
+require ("koneksi.php"); // Memanggil Koneksi.php
 
 $id_post = $_POST['id'];
 $nama_pengunjung_post = $_POST['nama_pengunjung'];
@@ -19,11 +18,26 @@ $tanggal_berkunjung_post = $_POST['tanggal_berkunjung'];
 $hobi_post = $_POST['hobi'];
 $disukai_post = $_POST['disukai'];
 
-$sql = "UPDATE pengunjung SET nama_pengunjung = '$nama_pengunjung_post', jenis_kelamin = '$jenis_kelamin_post', tanggal_berkunjung = '$tanggal_berkunjung_post', hobi = '$hobi_post', disukai = '$disukai_post' WHERE id = '$id_post'";
+$visitors = readVisitors();
+$updated = false;
+$old_data = [];
+$new_data = [
+    'id' => $id_post,
+    'nama_pengunjung' => $nama_pengunjung_post,
+    'jenis_kelamin' => $jenis_kelamin_post,
+    'tanggal_berkunjung' => $tanggal_berkunjung_post,
+    'hobi' => $hobi_post,
+    'disukai' => $disukai_post
+];
 
-
-$hasil = mysqli_query($conn, $sql);
-
+foreach ($visitors as $key => $visitor) {
+    if ($visitor['id'] == $id_post) {
+        $old_data = $visitors[$key]; // Simpan data lama untuk log
+        $visitors[$key] = $new_data; // Perbarui data
+        $updated = true;
+        break;
+    }
+}
 
 echo "<tr><td>ID Pengunjung</td><td>" . htmlspecialchars($id_post) . "</td></tr>";
 echo "<tr><td>Nama Pengunjung</td><td>" . htmlspecialchars($nama_pengunjung_post) . "</td></tr>";
@@ -36,13 +50,28 @@ echo "<tr><td>Disukai</td><td>" . htmlspecialchars($disukai_post) . "</td></tr>"
 <hr width="300">
 
 <?php
-if ($hasil) {
-    echo "Data Pengunjung Telah di Simpan";
-} else {
-    echo "Error menyimpan data: " . mysqli_error($conn);
-}
+if ($updated) {
+    writeVisitors($visitors); // Tulis kembali seluruh array ke file
 
-mysqli_close($conn);
+    // Detail perubahan untuk histori
+    $changes = [];
+    foreach ($old_data as $key => $value) {
+        if ($old_data[$key] != $new_data[$key]) {
+            $changes[] = "$key: '{$old_data[$key]}' menjadi '{$new_data[$key]}'";
+        }
+    }
+    $change_details = implode('; ', $changes);
+    if (empty($change_details)) {
+        $change_details = "Tidak ada perubahan data.";
+    }
+
+    // Log ke histori
+    logHistory('EDIT', $id_post, $nama_pengunjung_post, $change_details);
+
+    echo "Data Pengunjung Berhasil di Edit!";
+} else {
+    echo "Gagal mengedit data: ID Pengunjung " . htmlspecialchars($id_post) . " tidak ditemukan.";
+}
 ?>
 </center>
 </body>
