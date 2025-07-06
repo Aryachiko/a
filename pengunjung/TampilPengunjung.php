cbd50f31884b28717e31d51b4275d8d40ab6ed55
Aryachiko <arya08092023@gmail.com>
2025-07-06 19:10:28 +0700

    TampilPengunjung.php

--- pengunjung/TampilPengunjung.php
+++ pengunjung/TampilPengunjung.php
@@ -1,28 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tampil Data Pengunjung</title>
+    <link rel="stylesheet" href="w3.css">
+    <style>
+        table {
+            width: 80%;
+            border-collapse: collapse;
+            margin: 20px 0;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
 <center>
-<font size=7>
-Tampil Data Pengunjung<br>
+<h1>Data Pengunjung</h1>
 <hr>
 
-<table border="10">
-    <tr bgcolor="silver">
-        <td width="50"><center>Id</center></td>
-        <td width="150"><center>Nama Pengunjung</center></td>
-        <td width="100"><center>Jenis Kelamin</center></td>
-        <td width="150"><center>Tanggal Berkunjung</center></td>
-        <td width="100"><center>Hobi</center></td>
-        <td width="100"><center>Disukai</center></td>
-    </tr>
 <?php
-require ("Koneksi.php");
-$sql="SELECT * FROM pengunjung";
-$hasil = mysqli_query($conn, $sql);
-$n=1;
-while ($row = mysqli_fetch_row($hasil))
-{
-list ($id, $nama_pengunjung, $jenis_kelamin, $tanggal_berkunjung, $hobi, $disukai) = $row;
-echo "<tr><td>$n</td><td>$nama_pengunjung</td><td>$jenis_kelamin</td><td align=right>$tanggal_berkunjung</td><td align=right>$hobi</td><td>$disukai</td></tr>";
-$n++;
+require ("Koneksi.php"); // Memanggil Koneksi.php yang sekarang berisi fungsi file
+
+$visitors = readVisitors(); // Baca semua data pengunjung
+
+if (empty($visitors)) {
+    echo "<p>Belum ada data pengunjung.</p>";
+} else {
+    echo "<table class='w3-table-all w3-hoverable w3-card-4'>";
+    echo "<thead>";
+    echo "<tr class='w3-light-grey'>";
+    echo "<th>ID</th>";
+    echo "<th>Nama Pengunjung</th>";
+    echo "<th>Jenis Kelamin</th>";
+    echo "<th>Tanggal Berkunjung</th>";
+    echo "<th>Hobi</th>";
+    echo "<th>Disukai</th>";
+    echo "</tr>";
+    echo "</thead>";
+    echo "<tbody>";
+
+    foreach ($visitors as $visitor) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($visitor['id']) . "</td>";
+        echo "<td>" . htmlspecialchars($visitor['nama_pengunjung']) . "</td>";
+        echo "<td>" . htmlspecialchars($visitor['jenis_kelamin']) . "</td>";
+        echo "<td>" . htmlspecialchars($visitor['tanggal_berkunjung']) . "</td>";
+        echo "<td>" . htmlspecialchars($visitor['hobi']) . "</td>";
+        echo "<td>" . htmlspecialchars($visitor['disukai']) . "</td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
 }
 ?>
-</table>
 </center>
+</body>
+</html>
