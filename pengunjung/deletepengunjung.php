02378fa3a6dc215db9d79c2db424fb92643c2cc0
Aryachiko <arya08092023@gmail.com>
2025-07-06 19:12:07 +0700

    deletepengunjung.php

--- pengunjung/deletepengunjung.php
+++ pengunjung/deletepengunjung.php
@@ -9,13 +9,25 @@
 <hr width="300">
 <table>
 <?php
-require ("Koneksi.php");
+require ("Koneksi.php"); // Memanggil Koneksi.php
 
 $nama_pengunjung = $_POST['nama_pengunjung'];
+$deleted = false;
+$deletedVisitorId = '';
+$deletedVisitorName = '';
 
-$sql = "DELETE FROM pengunjung WHERE nama_pengunjung = '$nama_pengunjung'";
+$visitors = readVisitors();
+$newVisitors = [];
 
-$hasil = mysqli_query($conn, $sql);
+foreach ($visitors as $visitor) {
+    if ($visitor['nama_pengunjung'] !== $nama_pengunjung) {
+        $newVisitors[] = $visitor; // Tambahkan pengunjung yang tidak dihapus ke array baru
+    } else {
+        $deleted = true;
+        $deletedVisitorId = $visitor['id'];
+        $deletedVisitorName = $visitor['nama_pengunjung'];
+    }
+}
 
 echo "<tr><td>Nama Pengunjung</td><td>" . htmlspecialchars($nama_pengunjung) . "</td></tr>";
 ?>
@@ -23,17 +35,16 @@ echo "<tr><td>Nama Pengunjung</td><td>" . htmlspecialchars($nama_pengunjung) . "
 <hr width="300">
 
 <?php
-if ($hasil) {
-    if (mysqli_affected_rows($conn) > 0) {
-        echo "Data Pengunjung Telah di Hapus";
-    } else {
-        echo "Data Pengunjung dengan Nama " . htmlspecialchars($nama_pengunjung) . " tidak ditemukan atau sudah dihapus.";
-    }
-} else {
-    echo "Error menghapus data: " . mysqli_error($conn);
-}
+if ($deleted) {
+    writeVisitors($newVisitors); // Tulis kembali array yang sudah difilter ke file
 
-mysqli_close($conn);
+    // Log ke histori
+    logHistory('DELETE', $deletedVisitorId, $deletedVisitorName, 'Data dihapus');
+
+    echo "Data Pengunjung Telah di Hapus!";
+} else {
+    echo "Data Pengunjung dengan Nama " . htmlspecialchars($nama_pengunjung) . " tidak ditemukan atau sudah dihapus.";
+}
 ?>
 </center>
 </body>
